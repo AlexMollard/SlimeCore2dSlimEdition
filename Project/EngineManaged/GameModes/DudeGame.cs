1a87b0773a56de4ccfcb4692fc852563791d9e15
roryh012 <rory012.hunt@gmail.com>
2025-12-20 18:19:54 +1100

    Added IGameMode Interface with DudeGame.cs

---
+++ Project/EngineManaged/GameModes/DudeGame.cs
@@ -0,0 +1,24 @@
+﻿using SlimeCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlimeCore.GameModes;
+
+internal class DudeGame : IGameMode
+{
+    public static void Init()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void Shutdown()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void Update(float deltaTime)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Project/EngineManaged/Interfaces/IGameMode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlimeCore.Interfaces;
+
+internal interface IGameMode
+{
+    /// <summary>
+    /// Called once within ScriptRuntime.cs
+    /// </summary>
+    public abstract static void Init();
+    /// <summary>
+    /// This is called every frame within ScriptRuntime.cs
+    /// </summary>
+    /// <param name="deltaTime">The time between two frames</param>
+    public abstract static void Update(float deltaTime);
+    /// <summary>
+    /// This is called once when the game is shutting down from ScriptRuntime.cs
+    /// </summary>
+    public abstract static void Shutdown();
+}
