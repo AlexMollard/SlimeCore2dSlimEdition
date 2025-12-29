6f1b3b5ec3faf10c818cb12db8a751916fdae43e
Alex Mollard <alex.mollard@mail.com>
2025-12-29 13:38:48 +1100

    Added tile chunks and culling

--- Project/SlimeCore2D/Engine/Rendering/TileMap.h
+++ Project/SlimeCore2D/Engine/Rendering/TileMap.h
@@ -5,7 +5,7 @@
 #include <glm.hpp>
 #include "Rendering/Renderer2D.h"
 
-class TileMap
+class TileMapChunk
 {
 public:
     struct TileVertex
@@ -18,16 +18,17 @@ public:
         float IsText;
     };
 
-    TileMap(int width, int height, float tileSize);
-    ~TileMap();
+    TileMapChunk(int offsetX, int offsetY, int width, int height, float tileSize);
+    ~TileMapChunk();
 
     void SetTile(int x, int y, int layer, void* texturePtr, float r, float g, float b, float a);
     void UpdateMesh();
     void Render(const glm::mat4& viewProj);
+    bool IsVisible(const glm::mat4& viewProj);
 
 private:
-    int m_Width;
-    int m_Height;
+    int m_OffsetX, m_OffsetY;
+    int m_Width, m_Height;
     float m_TileSize;
 
     struct TileInfo
@@ -51,4 +52,27 @@ private:
     
     bool m_Dirty;
     uint32_t m_IndexCount;
+};
+
+class TileMap
+{
+public:
+    TileMap(int width, int height, float tileSize);
+    ~TileMap();
+
+    void SetTile(int x, int y, int layer, void* texturePtr, float r, float g, float b, float a);
+    void UpdateMesh();
+    void Render(const glm::mat4& viewProj);
+
+private:
+    int m_Width;
+    int m_Height;
+    float m_TileSize;
+
+    int m_ChunkWidth = 32;
+    int m_ChunkHeight = 32;
+    int m_ChunksX;
+    int m_ChunksY;
+
+    std::vector<TileMapChunk*> m_Chunks;
 };
